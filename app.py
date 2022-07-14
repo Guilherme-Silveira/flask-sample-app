64b5b51ea39451eca123238fd6ac786531e4997b
Guilherme-Silveira <guilhermesilveira.s@hotmail.com>
2022-07-13 22:11:05 -0300

    update app.py

--- app.py
+++ app.py
@@ -19,14 +19,16 @@ app.config['ELASTIC_APM'] = {
 'SERVICE_NAME': 'python-flask-app',
 
 # Use if APM Server requires a secret token
-'SECRET_TOKEN': '0McVQ01FNn3989g71m6UbtAB',
+'SECRET_TOKEN': 'HUxkxRDqm1D0sWjdd4',
 
 # Set the custom APM Server URL (default: http://localhost:8200)
-'SERVER_URL': 'http://apm-server-quickstart-apm-http:8200',
+'SERVER_URL': 'https://92ed1f715f4440a6b72897f383b6384d.apm.us-central1.gcp.cloud.es.io:443',
 
 # Set the service environment
-'ENVIRONMENT': 'production',
+'ENVIRONMENT': 'development'
+
 }
+#apm = ElasticAPM(app, logging=True)
 apm = ElasticAPM(app)
 
 @app.route('/')
@@ -35,4 +37,4 @@ def home():
   return jsonify({'message': 'Hello World'})
 
 if __name__=='__main__':
-  app.run(host='0.0.0.0', port=5000, debug=False)
+  app.run(host='0.0.0.0', port=5001, debug=False)
